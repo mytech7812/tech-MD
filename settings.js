b8b911d7e9e7abd1cc98ef1e64d77dc8286d4f64
mytech7812 <mytechjourney@aol.com>
2025-06-03 13:32:04 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight Bot',
-  author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  author: 'Bot‎',
+  botName: "5thStand Bot",
+  botOwner: 'Sam BOT', // Your name
+  ownerNumber: '18144968780', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
